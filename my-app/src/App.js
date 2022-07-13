ee5bf20de1bc860a132f0a9be44129607e1bb2cc
Sagar <dev.sagartmg@gmail.com>
2022-07-13 17:15:24 +0545

    init react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Fragment } from "react"
+
+// default export and named export
+// commonjs 
+// ES6 module system
+// arguement in function => props to component
+
+
+// return (
+//   {/*
+//   <div className="App">
+//      <header className="App-header">
+//       <p>{true ? "returnme ": "donot return me"}</p>
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <img src="./logo.svg" className="App-logo" alt="logo" />
+//       <img src="./logo192.png" className="App-logo" alt="logo" />
+//       <p>
+//         Edit <code>src/App.js</code> and save to reload.
+//       </p>
+//       <a
+//         className="App-link"
+//         href="https://reactjs.org"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         Learn React changed me
+//       </a>
+//     </header>
+//   </div>
+//    */}
+// );
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Fragment>
+    <div>welcome</div>
+    <div>welcome again</div>
+  </Fragment>
 }
 
 export default App;
